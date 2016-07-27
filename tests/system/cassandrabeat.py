01d1619658088b9e9f294dc7d365abb415cd3e86
Brandon Klise <bklise@goomzee.com>
2016-07-27 13:50:58 -0600

    Remove commented out code

--- tests/system/cassandrabeat.py
+++ tests/system/cassandrabeat.py
@@ -9,18 +9,6 @@ from datetime import datetime, timedelta
 
 build_path = "../../build/system-tests/"
 
-#import sys
-#sys.path.append('../../vendor/github.com/elastic/beats/libbeat/tests/system')
-#from beat.beat import TestCase
-#
-#class BaseTest(TestCase):
-#
-#    @classmethod
-#    def setUpClass(self):
-#        self.beat_name = "cassandrabeat"
-#        self.build_path = "../../build/system-tests/"
-#        self.beat_path = "../../cassandrabeat.test"
-
 class Proc(object):
     """
     Slim wrapper on subprocess.Popen that redirects 
